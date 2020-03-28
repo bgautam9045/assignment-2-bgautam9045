79b34fd6f405d4f6753dde1a85ec7a326c78de9c
bgautam9045 <bharti.gautam_cs18>
2020-03-28 16:43:55 +0530

    insert() function

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -16,5 +16,19 @@ public class MyPriorityQueue {
     public boolean isEmpty() {
         return front == null;
     }
+    public void insert(int data, int priority) {
+        Node n = new Node(data, priority);
+        if (isEmpty() || n.getPriority() < front.getPriority()) {
+            n.setNext(front);
+            front = n;
+        } else {
+            Node tem = front;
+            while (tem.getNext() != null && tem.getNext().getPriority() <= n.getPriority()) {
+                tem = tem.getNext();
+            }
+            n.setNext(tem.getNext());
+            tem.setNext(n);
+        }
+    }
 
 }
