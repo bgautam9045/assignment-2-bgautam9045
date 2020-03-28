5e8cfe7ffd340a691aa5c5aa2b57a619adfafe08
bgautam9045 <bharti.gautam_cs18>
2020-03-28 16:57:03 +0530

    getter and setter of MyQueue class

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -27,4 +27,30 @@ public class MyQueue {
         queue.tmp = queue.front;
         return size;
     }
+    public void setSize(int size) {
+        this.size = size;
+    }
+    public Node getFront() {
+        return front;
+    }
+
+    public void setFront(Node front) {
+        this.front = front;
+    }
+
+    public Node getEnd() {
+        return end;
+    }
+
+    public void setEnd(Node end) {
+        this.end = end;
+    }
+
+    public Node getTmp() {
+        return tmp;
+    }
+
+    public void setTmp(Node tmp) {
+        this.tmp = tmp;
+    }
 }
