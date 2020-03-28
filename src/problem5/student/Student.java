8f1a54d67042a2beee2efa9f09309371491023ec
bgautam9045 <bharti.gautam_cs18>
2020-03-28 17:07:18 +0530

    student constructor

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -6,5 +6,20 @@
  */
 package problem5.student;
 // to store student information and properties
+import java.util.Scanner;
 public class Student {
+    private String name;
+    private int rollno, backlog, apperingcount;
+    private Scanner sc;
+    public Student() {
+        sc = new Scanner(System.in);
+        System.out.print("Name :");
+        this.name = sc.next();
+        System.out.print("Roll.no : ");
+        this.rollno = sc.nextInt();
+        System.out.print("Backlog_count : ");
+        this.backlog = sc.nextInt();
+        System.out.print("appering_count");
+        this.apperingcount = sc.nextInt();
+    }
 }
