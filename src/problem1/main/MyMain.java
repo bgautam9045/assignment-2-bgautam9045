3df408593990c65e012f80495a5bb6a3d7dd1fd1
bgautam9045 <bharti.gautam_cs18>
2020-03-28 16:09:50 +0530

    main class

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -6,6 +6,24 @@
  */
 package problem1.main;
 // executable class
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
 
+public abstract class MyMain {
+    public static void main(String [] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        TreeNode root;
+        root = new TreeNode(7);
+        obj.insert(root , 6);
+        obj.insert(root , 3);
+        obj.insert(root , 5);
+        obj.insert(root , 7);
+        obj.insert(root , 9);
+        obj.insert(root , 2);
+        obj.insert(root , 1);
+        obj.leftView(root);
+
+    }
+}
 
 
