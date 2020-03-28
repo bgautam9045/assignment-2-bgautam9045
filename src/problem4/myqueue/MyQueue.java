063ca264e2dfdf69de76cf74ea3eb9ac2fb3dcde
bgautam9045 <bharti.gautam_cs18>
2020-03-28 16:55:00 +0530

    MyQueue constructor

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -6,6 +6,17 @@
  */
 package problem4.myqueue;
 // to create queue to store pre - order successor
-
+import problem1.node.TreeNode;
 public class MyQueue {
+    private Node front, end, tmp;
+    private int size;
+
+
+    public MyQueue() {
+        front = null;
+        end = null;
+        tmp = null;
+        size = 0;
+    }
+
 }
