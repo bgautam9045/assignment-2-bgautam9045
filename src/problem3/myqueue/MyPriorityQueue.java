185cfb5100a9a98f550f93b130ab68c2c2adf159
bgautam9045 <bharti.gautam_cs18>
2020-03-28 16:42:46 +0530

    MyPriorityQueue constructor

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -5,6 +5,13 @@
  *  Time: 7:15 PM
  */
 package problem3.myqueue;
-
+import problem3.node.Node;
 public class MyPriorityQueue {
+    private Node front;
+    private Node rear;
+    public MyPriorityQueue() {
+        this.front = null;
+        this.rear = null;
+    }
+
 }
