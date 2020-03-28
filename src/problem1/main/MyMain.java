aeae9cdc7ee001a764fd0c18e111f16bd956f0a8
bgautam9045 <bharti.gautam_cs18>
2020-03-28 15:54:35 +0530

    treenode constructor

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,5 @@
 package problem1.main;
 // executable class
 
-public class MyMain {
-}
+
+
--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -7,4 +7,12 @@
 package problem1.node;
 
 public class TreeNode {
+    private int data;
+    private TreeNode left;
+    private TreeNode right;
+    public TreeNode(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
 }
