4d162b36fb99598b0b6eb0b6d489eb55b76dd5ff
bgautam9045 <bharti.gautam_cs18>
2020-03-28 16:39:02 +0530

    node constructor

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -7,4 +7,12 @@
 package problem3.node;
 
 public class Node {
+    private int data;
+    private Node next;
+    private int priority;
+    public Node(int data , int priority) {
+        this.data = data;
+        this.next = null;
+        this.priority = priority;
+    }
 }
