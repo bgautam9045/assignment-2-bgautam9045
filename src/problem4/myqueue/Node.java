6aecf03c674b27ea02eb5760ed28d9fba0bf3562
bgautam9045 <bharti.gautam_cs18>
2020-03-28 16:51:08 +0530

    node constructor

---
+++ src/problem4/myqueue/Node.java
@@ -0,0 +1,15 @@
+package problem4.myqueue;
+import problem1.node.TreeNode;
+
+public class Node {
+    private TreeNode node;
+    private  Node next;
+    public Node(int data) {
+        node = new TreeNode(data);
+        next = null;
+    }
+    public Node(TreeNode treenode) {
+        node = treenode;
+        next = null;
+    }
+}
