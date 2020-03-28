42347584761c53670a835eaa765799adde8150c3
bgautam9045 <bharti.gautam_cs18>
2020-03-28 15:58:49 +0530

    method to insert element in a tree

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -12,4 +12,16 @@ import java.util.LinkedList;
 import java.util.Queue;
 public class MyBinarySearchTree {
     static Queue<TreeNode> q;
+    //constructor
+
+    //method to insert a element in tree
+    public TreeNode insert(TreeNode root, int x) {
+        if (root == null)
+            return new TreeNode(x);
+        else if (x > root.getData()) // x is greater. Should be inserted to right
+            root.setRight(insert(root.getRight(), x));
+        else // x is smaller should be inserted to left
+            root.setLeft(insert(root.getLeft(), x));
+        return root;
+    }
 }
